babe0185e7009e6d784f2b4dfe263dca75a0d471
wangjun <444055828@qq.com>
2023-09-23 23:55:53 +0800

    add .gitignore

--- target/debug/deps/from_env-59544ee65f8c4efa.d
+++
@@ -1,5 +0,0 @@
-/home/wangwenqing/hobby/rust/from-env-derive/target/debug/deps/from_env-59544ee65f8c4efa.rmeta: /home/wangwenqing/.cargo/git/checkouts/from-env-rs-f88ccefdc10ff247/ab1e13e/src/lib.rs
-
-/home/wangwenqing/hobby/rust/from-env-derive/target/debug/deps/from_env-59544ee65f8c4efa.d: /home/wangwenqing/.cargo/git/checkouts/from-env-rs-f88ccefdc10ff247/ab1e13e/src/lib.rs
-
-/home/wangwenqing/.cargo/git/checkouts/from-env-rs-f88ccefdc10ff247/ab1e13e/src/lib.rs:
--- target/debug/deps/from_env_derive-b97ce0f4ec88fea9.d
+++
@@ -1,9 +0,0 @@
-/home/wangwenqing/hobby/rust/from-env-derive/target/debug/deps/from_env_derive-b97ce0f4ec88fea9.rmeta: src/lib.rs Cargo.toml
-
-/home/wangwenqing/hobby/rust/from-env-derive/target/debug/deps/from_env_derive-b97ce0f4ec88fea9.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
